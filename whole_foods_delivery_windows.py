ed02c2018e35d742926b432393073197ad91545d
alex-levin <alexlevinb@gmail.com>
2020-04-18 10:46:39 -0400

    Fixed whole_foods_delivery_slot_chrome.py: removed unused variables, removed the need to download Chrome driver binary

--- whole_foods_delivery_windows.py
+++ whole_foods_delivery_windows.py
@@ -9,45 +9,47 @@ import winsound
 
 
 def getWFSlot(productUrl):
-   # driver = webdriver.Chrome()
-   driver = webdriver.Chrome(ChromeDriverManager().install())
-   driver.get(productUrl)           
-   html = driver.page_source
-   soup = bs4.BeautifulSoup(html, "html.parser")
-   time.sleep(60)
-   no_open_slots = True
-        
-   duration = 10000
-   freq = 440
+    # driver = webdriver.Chrome()
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get(productUrl)
+    html = driver.page_source
+    soup = bs4.BeautifulSoup(html, "html.parser")
+    time.sleep(60)
+    no_open_slots = True
 
-   while no_open_slots:
-      driver.refresh()
-      print("refreshed")
-      html = driver.page_source
-      soup = bs4.BeautifulSoup(html, "html.parser")
-      time.sleep(4)
+    duration = 10000
+    freq = 440
 
-      slot_pattern = 'Next available'
-      try:
-         next_slot_text = soup.find('h4', class_ ='ufss-slotgroup-heading-text a-text-normal').text
-         if slot_pattern in next_slot_text:
+    while no_open_slots:
+
+
+driver.refresh()
+print("refreshed")
+html = driver.page_source
+soup = bs4.BeautifulSoup(html, "html.parser")
+ time.sleep(4)
+
+  slot_pattern = 'Next available'
+   try:
+        next_slot_text = soup.find(
+            'h4', class_='ufss-slotgroup-heading-text a-text-normal').text
+        if slot_pattern in next_slot_text:
             print('SLOTS OPEN!')
             winsound.Beep(freq, duration)
             no_open_slots = False
             time.sleep(1400)
-      except AttributeError:
-         continue
+    except AttributeError:
+        continue
 
-      try:
-         no_slot_pattern = 'No delivery windows available. New windows are released throughout the day.'
-         if no_slot_pattern == soup.find('h4', class_ ='a-alert-heading').text:
+    try:
+        no_slot_pattern = 'No delivery windows available. New windows are released throughout the day.'
+        if no_slot_pattern == soup.find('h4', class_='a-alert-heading').text:
             print("NO SLOTS!")
-      except AttributeError: 
-            print('SLOTS OPEN!')
-            winsound.Beep(freq, duration)
-            no_open_slots = False
+    except AttributeError:
+        print('SLOTS OPEN!')
+        winsound.Beep(freq, duration)
+        no_open_slots = False
 
 
 getWFSlot('https://www.amazon.com/gp/buy/shipoptionselect/handlers/display.html?hasWorkingJavascript=1')
-
 

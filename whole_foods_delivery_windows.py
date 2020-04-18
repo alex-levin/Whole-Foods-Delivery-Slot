9b3abf4c402a3c16a0ac44bc16c42e664da8b330
alex-levin <alexlevinb@gmail.com>
2020-04-18 11:44:12 -0400

    fixes versions

--- whole_foods_delivery_windows.py
+++ whole_foods_delivery_windows.py
@@ -21,35 +21,32 @@ def getWFSlot(productUrl):
     freq = 440
 
     while no_open_slots:
+        driver.refresh()
+        print("refreshed")
+        html = driver.page_source
+        soup = bs4.BeautifulSoup(html, "html.parser")
+        time.sleep(4)
 
+        slot_pattern = 'Next available'
+        try:
+            next_slot_text = soup.find(
+                'h4', class_='ufss-slotgroup-heading-text a-text-normal').text
+            if slot_pattern in next_slot_text:
+                print('SLOTS OPEN!')
+                winsound.Beep(freq, duration)
+                no_open_slots = False
+                time.sleep(1400)
+        except AttributeError:
+            continue
 
-driver.refresh()
-print("refreshed")
-html = driver.page_source
-soup = bs4.BeautifulSoup(html, "html.parser")
- time.sleep(4)
-
-  slot_pattern = 'Next available'
-   try:
-        next_slot_text = soup.find(
-            'h4', class_='ufss-slotgroup-heading-text a-text-normal').text
-        if slot_pattern in next_slot_text:
+        try:
+            no_slot_pattern = 'No delivery windows available. New windows are released throughout the day.'
+            if no_slot_pattern == soup.find('h4', class_='a-alert-heading').text:
+                print("NO SLOTS!")
+        except AttributeError:
             print('SLOTS OPEN!')
             winsound.Beep(freq, duration)
             no_open_slots = False
-            time.sleep(1400)
-    except AttributeError:
-        continue
-
-    try:
-        no_slot_pattern = 'No delivery windows available. New windows are released throughout the day.'
-        if no_slot_pattern == soup.find('h4', class_='a-alert-heading').text:
-            print("NO SLOTS!")
-    except AttributeError:
-        print('SLOTS OPEN!')
-        winsound.Beep(freq, duration)
-        no_open_slots = False
 
 
 getWFSlot('https://www.amazon.com/gp/buy/shipoptionselect/handlers/display.html?hasWorkingJavascript=1')
-
